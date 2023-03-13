2105ac9505917fccc1622f936bb72773bf028e62
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2023-03-13 16:48:18 -0700

    Dynamically generates nav items.

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,16 +1,31 @@
 import "./css/Header.css";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { ITodoData } from "../main/TodoList";
+import { getToDoList } from "../ExternalServices";
+
+function NavItem({ name, active = false }): JSX.Element {
+	return (
+		<li>
+			<a href="src#" className={active ? "active" : ""}>
+				{name}
+			</a>
+		</li>
+	);
+}
 
 function Header() {
+	const [todoList, setTodoList] = useState<ITodoData[]>([]);
+	const [active, setActive] = useState("all");
+
 	useEffect(() => {
 		// Hide header on scroll
-		let prevScrollpos = window.scrollY;
-		const header = document.querySelector("header");
-		const search_bar = document.getElementById(
+		let prevScrollpos: number = window.scrollY;
+		const header: Element = document.querySelector("header");
+		const search_bar: Element = document.getElementById(
 			"search-bar-container-container"
 		);
 		window.onscroll = function () {
-			const currentScrollPos = window.scrollY;
+			const currentScrollPos: number = window.scrollY;
 			if (prevScrollpos > currentScrollPos) {
 				header.classList.remove("hide");
 				search_bar.classList.remove("hide");
@@ -22,22 +37,33 @@ function Header() {
 		};
 	}, []);
 
+	// To get the data
+	useEffect(() => {
+		const fetchTodoList = async () => {
+			const fetchedList: ITodoData[] = await getToDoList();
+			setTodoList(fetchedList);
+		};
+		fetchTodoList();
+	}, []);
+
+	// Gets the unique types from todoList
+	const uniqueTypes: string[] = Array.from(
+		new Set(todoList.map((todo: ITodoData) => todo.type.toLowerCase()))
+	);
+	uniqueTypes.unshift("all");
+
 	return (
 		<header>
 			<div className="container">
 				<nav>
 					<ul>
-						<li>
-							<a href="src#" className="active">
-								All
-							</a>
-						</li>
-						<li>
-							<a href="src#">General</a>
-						</li>
-						<li>
-							<a href="src#">Health</a>
-						</li>
+						{uniqueTypes.map((type) => (
+							<NavItem
+								key={type}
+								name={type}
+								active={type === active}
+							/>
+						))}
 					</ul>
 				</nav>
 			</div>

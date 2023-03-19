ad424ca7fae6d1848c616e0ddf5d4fa206cec134
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2023-03-19 01:59:28 -0700

    Added sleep utils function

--- src/utils.ts
+++ src/utils.ts
@@ -21,3 +21,7 @@ export function clickAnywhere(elements: Element[], next): void {
 		}
 	});
 }
+
+export function sleep(ms): Promise<void> {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}

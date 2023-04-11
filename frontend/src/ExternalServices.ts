2dbb40e2d44139e2d322032647ef9323740dfed1
DanielGRasmussen <104102130+DanielGRasmussen@users.noreply.github.com>
2023-04-11 15:22:41 -0700

    Added stuff to prevent API functions from being called while another is running. Also made them store info in session.

--- frontend/src/ExternalServices.ts
+++ frontend/src/ExternalServices.ts
@@ -1,5 +1,6 @@
 import ITodoData from "./DataInterfaces";
 import axios from "axios";
+import { sleep } from "./utils";
 
 export async function getToDoList(): Promise<ITodoData[]> {
 	const response = await axios.get("/api/todos", { maxRedirects: 0 });
@@ -19,37 +20,81 @@ export function getTodoByIdFromLocal(id: string): ITodoData {
 	}
 }
 
+let isExecuting = false;
 export async function saveNewTodo(todoData: ITodoData): Promise<void> {
+	if (isExecuting) {
+		await sleep(1000);
+		return saveNewTodo(todoData);
+	}
+	isExecuting = true; // Got to prevent a race condition
+
+	const sessionData = sessionStorage.getItem("todos");
+	let todos = [];
+	if (sessionData) {
+		try {
+			todos = JSON.parse(sessionData);
+		} catch (error) {
+			console.error("Error parsing session data:", error);
+		}
+	}
+	todos.push(todoData);
+	sessionStorage.setItem("todos", JSON.stringify(todos));
+
 	await axios.post("/api/todos", todoData, { maxRedirects: 0 });
-	// const todoList = JSON.parse(sessionStorage.getItem("todos"));
-	// todoList.push(todoData);
-	// sessionStorage.setItem("todos", JSON.stringify(todoList));
+	isExecuting = false;
 	return;
 }
 
 export async function saveTodo(todoData: ITodoData): Promise<void> {
+	if (isExecuting) {
+		await sleep(1000);
+		return saveTodo(todoData);
+	}
+	isExecuting = true; // Got to prevent a race condition
+
+	const sessionData = sessionStorage.getItem("todos");
+	let todos = [];
+	if (sessionData) {
+		try {
+			todos = JSON.parse(sessionData);
+		} catch (error) {
+			console.error("Error parsing session data:", error);
+		}
+	}
+	// Modify the object in the array that matches the _id property of todoData
+	const todoIndex = todos.findIndex((todo) => todo._id === todoData._id);
+	if (todoIndex !== -1) todos[todoIndex] = todoData;
+
+	sessionStorage.setItem("todos", JSON.stringify(todos));
 	await axios.put(`/api/todos/${todoData._id}`, todoData, { maxRedirects: 0 });
-	// const todoList = JSON.parse(sessionStorage.getItem("todos")) as ITodoData[];
-	// for (let todoItem of todoList) {
-	// 	if (todoItem._id === todoData._id) {
-	// 		todoItem = todoData;
-	// 		sessionStorage.setItem("todos", JSON.stringify(todoList));
-	// 		break;
-	// 	}
-	// }
+	isExecuting = false;
 	return;
 }
 
 export async function deleteTodoById(id: string): Promise<void> {
-	await axios.delete(`/api/todos/${id}`, { maxRedirects: 0 });
-	const todoList = JSON.parse(sessionStorage.getItem("todos")) as ITodoData[];
-	for (let i = 0; i < todoList.length; i++) {
-		if (todoList[i]._id === id) {
-			todoList.splice(i, 1);
-			sessionStorage.setItem("todos", JSON.stringify(todoList));
-			break;
+	if (isExecuting) {
+		await sleep(1000);
+		return deleteTodoById(id);
+	}
+	isExecuting = true; // Got to prevent a race condition
+
+	const sessionData = sessionStorage.getItem("todos");
+	let todos = [];
+	if (sessionData) {
+		try {
+			todos = JSON.parse(sessionData);
+		} catch (e) {
+			console.error("Error parsing session data:", e);
 		}
 	}
+
+	// Find the index of the object in the array that matches the id and delete it
+	const todoIndex = todos.findIndex((t) => t._id === id);
+	if (todoIndex !== -1) todos.splice(todoIndex, 1);
+	sessionStorage.setItem("todos", JSON.stringify(todos));
+
+	await axios.delete(`/api/todos/${id}`, { maxRedirects: 0 });
+	isExecuting = false;
 	return;
 }
 
@@ -72,12 +117,13 @@ export async function getUserInfo(): Promise<{
 		};
 	else userInfo = response.data;
 	sessionStorage.setItem("userInfo", JSON.stringify(userInfo));
+	isExecuting = false;
 	return userInfo;
 }
 
 export async function logout(): Promise<void> {
-	await axios.get("/api/auth/logout)");
 	sessionStorage.removeItem("userInfo");
 	sessionStorage.removeItem("todos");
+	await axios.get("/api/auth/logout)");
 	return;
 }
